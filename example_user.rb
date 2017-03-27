d0e63f6da4d9dc426d5b6687a6d6492a994397fa
Marta Kowalska <marta.kowalska.987@gmail.com>
2017-03-28 01:59:36 +0200

    update example-user for testing

--- example_user.rb
+++ example_user.rb
@@ -1,12 +1,22 @@
 class User
-  attr_accessor :name, :email
+  attr_accessor :name_first, :name_last, :email
 
   def initialize(attributes = {})
-    @name  = attributes[:name]
+    @name_first  = attributes[:name_first]
+    @name_last  = attributes[:name_last]
     @email = attributes[:email]
   end
 
-  def formatted_email
-    "#{@name} <#{@email}>"
+  def full_name
+    @name_first + " " + @name_last
   end
+
+  def alp_name
+    @name_last + "," + @name_first
+  end
+
+  def formatted_email
+    "#{@name_first} <#{@email}>"
+  end
+
 end
